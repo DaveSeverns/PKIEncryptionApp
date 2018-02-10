919a17ed8e003367ba2f6fab17fe7891d04f4bf5
daveseverns <david.w.severns@gmail.com>
2018-02-10 14:44:40 -0500

    wiring up buttons

--- app/src/main/java/com/sevdev/pkiencryptionapp/ClientActivity.java
+++ app/src/main/java/com/sevdev/pkiencryptionapp/ClientActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ClientActivity extends AppCompatActivity {
 
@@ -16,12 +17,18 @@ public class ClientActivity extends AppCompatActivity {
     Button encryptButton;
     EditText editText;
     String textToEncrypt;
+    String pubKey;
+    String privKey;
+    boolean keysAvailable;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+        keysAvailable = false; // set to false if no keys generated
+        editText = findViewById(R.id.editText);
 
         findViewById(R.id.generateKeyButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +37,26 @@ public class ClientActivity extends AppCompatActivity {
                         null,null,null,null);
 
                 cursor.moveToNext();
+                pubKey = cursor.getString(0);
+                privKey = cursor.getString(1);
+                keysAvailable = true;
                 Log.e("public key", cursor.getString(0));
                 Log.e("private key", cursor.getString(1));
+            }
+        });
+
+        findViewById(R.id.encryptButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!keysAvailable){
+                    Toast.makeText(ClientActivity.this, "Need to generate keys", Toast.LENGTH_SHORT).show();
+                }
+                else if(editText.getText().toString().equals(null)){
+                    Toast.makeText(ClientActivity.this, "Enter text to encrypt", Toast.LENGTH_SHORT).show();
+                }
+                else{
+
+                }
             }
         });
 
