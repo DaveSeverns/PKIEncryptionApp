e426fad22449afd3c4e0d82c85a6242c651313e0
daveseverns <david.w.severns@gmail.com>
2018-02-23 00:51:18 -0500

    moving along

---
+++ app/src/main/java/com/sevdev/pkiencryptionapp/ReadActivity.java
@@ -0,0 +1,32 @@
+package com.sevdev.pkiencryptionapp;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReadActivity extends AppCompatActivity {
+
+    PendingIntent pendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+        Intent intent = new Intent(this, ReadActivity.class);
+        pendingIntent = PendingIntent.getActivity(this,0, intent, 0);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        NfcAdapter.getDefaultAdapter(this).enableForegroundDispatch(this,pendingIntent,null, null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        NfcAdapter.getDefaultAdapter(this).disableForegroundDispatch(this);
+    }
+}
